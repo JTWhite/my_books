6bc6d38c66295dd13176d27f1e35a77a7aa90fe1
JTWhite <james.twhite93@gmail.com>
2023-01-14 21:03:33 +0000

    basic flask template setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
 
-from app import views 
+app = Flask(__name__, template_folder="../templates")
+
+from app import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from app import app
+from flask import render_template
+
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
 
 @app.route("/about")
-def index():
+def about():
     return "About page"
