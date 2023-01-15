854d6b26b64f3d5dbcb7ca646b53dbf490441bb5
JTWhite <james.twhite93@gmail.com>
2023-01-15 10:04:03 +0000

    added blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask, url_for
+from flask import Flask
+from .views.home import home
+from .views.admin import admin
 
-
-app = Flask(
-    __name__, template_folder="../templates", static_folder="../static"
-)
-
-from app import views
+app = Flask(__name__)
+app.config.from_object("config")
+app.register_blueprint(home)
+app.register_blueprint(admin)
---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG = True  # Turns on debugging features in Flask
+BCRYPT_LOG_ROUNDS = 12  # Configuration for the Flask-Bcrypt extension
+MAIL_FROM_EMAIL = "james.twhit@yahoo.co.uk"  # For use in application emails
+GOOGLE_API_KEY = "AIzaSyDyckQNwImbPMwNvGzaHLlnk31m7k9eOV0"
